83462964a8d19f4efc51f0735b2e86d985d84145
holybobrius <ilya.povaroff@gmail.com>
2021-01-10 00:52:29 +0300

    Linted newTask.js

--- src/newTask.js
+++ src/newTask.js
@@ -1,88 +1,89 @@
-import {buildNewTask, buildNewList, getListIndex, getTaskIndex, deleteTaskDOM, checkTaskDOM} from "./DOMBuilding";
+/* eslint-disable max-len */
+import {
+  buildNewTask, buildNewList, getListIndex, getTaskIndex, deleteTaskDOM, checkTaskDOM,
+} from './DOMBuilding';
 
-const nameField = document.querySelector("#popup-task-name-input");
+const nameField = document.querySelector('#popup-task-name-input');
 const descriptionField = document.querySelector('#popup-task-description-input');
 const dueDateField = document.querySelector('#popup-task-duedate-input');
 const priorityField = document.querySelector('#popup-task-priority-input');
-const newTaskPopup = document.querySelector(".new-task-popup");
-let tasks = document.querySelector('#tasks');
+const newTaskPopup = document.querySelector('.new-task-popup');
+const tasks = document.querySelector('#tasks');
 
-
-let listsArr = [];
+const listsArr = [];
 
 class Task {
-    constructor(name, description, dueDate, priority, checked) {
-        this.name = name;
-        this.description = description;
-        this.dueDate = dueDate;
-        this.priority = priority;
-        this.checked = checked;
-    }
+  constructor(name, description, dueDate, priority, checked) {
+    this.name = name;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.checked = checked;
+  }
 }
 
 class List {
-    constructor(name) {
-        this.name = name;
-        this.content = [];
-    }
+  constructor(name) {
+    this.name = name;
+    this.content = [];
+  }
 
-    addTaskToList(task) { 
-        this.content.push(task);
-    }
+  addTaskToList(task) {
+    this.content.push(task);
+  }
 
-    render() {
-        for(let i = 0; i < this.content.length; i++) {
-            buildNewTask(this.content[i].name, this.content[i].description, this.content[i].dueDate, this.content[i].priority, this.content[i].checked);
-        } 
+  render() {
+    for (let i = 0; i < this.content.length; i++) {
+      buildNewTask(this.content[i].name, this.content[i].description, this.content[i].dueDate, this.content[i].priority, this.content[i].checked);
     }
+  }
 }
 
-let currentList = new List("Default");
+let currentList = new List('Default');
 listsArr.push(currentList);
 
 function addTask() {
-    if(nameField.value != "" && descriptionField.value != "" && dueDateField.value != "" && priorityField.value != "") {
-        let newTask = new Task(nameField.value, descriptionField.value, dueDateField.value, priorityField.value, false);
-        currentList.content.push(newTask);
-        buildNewTask(newTask.name, newTask.description, newTask.dueDate, newTask.priority, newTask.checked);
-        newTaskPopup.style.display = "none";
-    }
+  if (nameField.value != '' && descriptionField.value != '' && dueDateField.value != '' && priorityField.value != '') {
+    const newTask = new Task(nameField.value, descriptionField.value, dueDateField.value, priorityField.value, false);
+    currentList.content.push(newTask);
+    buildNewTask(newTask.name, newTask.description, newTask.dueDate, newTask.priority, newTask.checked);
+    newTaskPopup.style.display = 'none';
+  }
 }
 
 const listNameField = document.querySelector('#popup-list-name-input');
 const newListPopup = document.querySelector('.new-list-popup');
 
-
-
 function deleteTask(task) {
-    currentList.content.splice(getTaskIndex(task), 1);
-    deleteTaskDOM(task);
+  currentList.content.splice(getTaskIndex(task), 1);
+  deleteTaskDOM(task);
 }
 
-
 function addList() {
-    if(listNameField.value != "") {
-        let newList = new List(listNameField.value);
-        buildNewList(newList.name);
-        listsArr.push(newList);
-        console.table(`Lists array: ${listsArr}`);
-        newListPopup.style.display = "none";
-    }
+  if (listNameField.value != '') {
+    const newList = new List(listNameField.value);
+    buildNewList(newList.name);
+    listsArr.push(newList);
+    console.table(`Lists array: ${listsArr}`);
+    newListPopup.style.display = 'none';
+  }
 }
 
 function selectCurrentList(list) {
-    currentList = listsArr[getListIndex(list)];
-    tasks.innerHTML = '';
-    currentList.render();
+  currentList = listsArr[getListIndex(list)];
+  tasks.innerHTML = '';
+  currentList.render();
 }
 
-function checkTask(task) { 
-    if(currentList.content[getTaskIndex(task)].checked === false) {
-        currentList.content[getTaskIndex(task)].checked = true;
-    } else {
-        currentList.content[getTaskIndex(task)].checked = false;
-    }
-    checkTaskDOM(task);
+function checkTask(task) {
+  if (currentList.content[getTaskIndex(task)].checked === false) {
+    currentList.content[getTaskIndex(task)].checked = true;
+  } else {
+    currentList.content[getTaskIndex(task)].checked = false;
+  }
+  checkTaskDOM(task);
 }
 
-export {addList, addTask, selectCurrentList, deleteTask, checkTask}
+export {
+  addList, addTask, selectCurrentList, deleteTask, checkTask,
+};
